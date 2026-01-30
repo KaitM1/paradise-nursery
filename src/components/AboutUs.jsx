470d2e630a91eaf6f9069dbef8ad5a03b02278f0
unknown <marciosribeiro01@outlook.com>
2026-01-30 00:35:04 -0300

    add aboutus component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant store dedicated to bringing
+        high-quality plants to your home. We offer a wide variety of indoor
+        and outdoor plants, carefully selected to help you create a greener
+        and healthier environment.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
