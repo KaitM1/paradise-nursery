b278eb04d383253d8bf9d4fba92f0848ccd2412d
unknown <marciosribeiro01@outlook.com>
2026-01-30 00:51:32 -0300

    Add landing page with company name and start button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Paradise Nursery</h1>
+      <button>Começar</button>
+    </div>
+  );
+}
+
+export default App;
